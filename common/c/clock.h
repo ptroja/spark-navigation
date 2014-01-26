d836821347c8278fb0198ae02d004cfc8f9a7fb7
Piotr Trojanek <piotr.trojanek@gmail.com>
2014-01-26 23:27:06 +0000

    clock

--- common/c/clock.h
+++ common/c/clock.h
@@ -3,6 +3,10 @@
 
 #include <time.h>
 
+#ifdef __cplusplus /* If this is a C++ compiler, use C linkage */
+extern "C" {
+#endif
+
 struct timespec
 evAddTime(struct timespec addend1, struct timespec addend2);
 
@@ -11,5 +15,9 @@ evSubTime(struct timespec minuend, struct timespec subtrahend);
 
 struct timespec
 evNowTime(void);
+
+#ifdef __cplusplus /* If this is a C++ compiler, end C linkage */
+}
+#endif
 
 #endif /* _CLOCK_H */
