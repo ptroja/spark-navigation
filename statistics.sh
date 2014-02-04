c6a0e81a45847869b4bfbcd1630f51818b754c8b
Piotr Trojanek <piotr.trojanek@gmail.com>
2014-02-04 15:14:15 +0000

    Ada.Containers.Count_Type in overflow statistics

--- statistics.sh
+++ statistics.sh
@@ -20,7 +20,7 @@ echo Total: ${OK_SUM}/${SUM}
 # LaTeX table output
 echo "LaTeX table line:"
 for i in precondition postcondition "loop invariant" "loop variant" assertion division int_overflow float_overflow range index; do
-  CNT=`grep "${i}" ${FILE} | wc -l`
+  CNT=`grep "${i}" ${FILE} | grep -v "saved VC" | wc -l`
   echo -n " & ${CNT}"
 done
 
@@ -29,10 +29,12 @@ echo
 
 echo
 
-FLOAT_OVERFLOWS=`grep "\"GP_Reason:VC_OVERFLOW_CHECK\" \"GP_Sloc_VC:[a-zA-Z_]\+.ad[bs]:[0-9]\+:[0-9]\+\" \"keep_on_simp\" (Standard__float" lib/obj/gnatprove/*.mlw -o |uniq|wc -l`
-INTEGER_OVERFLOWS=`grep "\"GP_Reason:VC_OVERFLOW_CHECK\" \"GP_Sloc_VC:[a-zA-Z_]\+.ad[bs]:[0-9]\+:[0-9]\+\" \"keep_on_simp\" (Standard__integer" lib/obj/gnatprove/*.mlw -o |uniq|wc -l`
+INTEGER_OVERFLOWS=`grep "\"GP_Reason:VC_OVERFLOW_CHECK\" \"GP_Sloc_VC:[a-zA-Z_]\+.ad[bs]:[0-9]\+:[0-9]\+\" \"keep_on_simp\" (Standard__integer" algorithm/obj/gnatprove/*.mlw -o |uniq|wc -l`
+CONTAINER_OVERFLOWS=`grep "\"GP_Reason:VC_OVERFLOW_CHECK\" \"GP_Sloc_VC:[a-zA-Z_]\+.ad[bs]:[0-9]\+:[0-9]\+\" \"keep_on_simp\" (Ada__container" algorithm/obj/gnatprove/*.mlw -o |uniq|wc -l`
+FLOAT_OVERFLOWS=`grep "\"GP_Reason:VC_OVERFLOW_CHECK\" \"GP_Sloc_VC:[a-zA-Z_]\+.ad[bs]:[0-9]\+:[0-9]\+\" \"keep_on_simp\" (Standard__float" algorithm/obj/gnatprove/*.mlw -o |uniq|wc -l`
 
 echo "integer overflows: ${INTEGER_OVERFLOWS}"
+echo "container overflows: ${CONTAINER_OVERFLOWS}"
 echo "floating-point overflows: ${FLOAT_OVERFLOWS}"
 TOTAL_OVERFLOWS=`expr ${INTEGER_OVERFLOWS} + ${FLOAT_OVERFLOWS}`
 echo "total overflows: ${TOTAL_OVERFLOWS}"
