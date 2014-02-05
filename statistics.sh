959d060af68f43c5281f8b5863c3422e08aa326e
Piotr Trojanek <piotr.trojanek@gmail.com>
2014-02-05 00:17:48 +0000

    statistics: discriminants and count_type overflows

--- statistics.sh
+++ statistics.sh
@@ -6,7 +6,7 @@ OK_SUM=0
 
 # Textual output
 echo "VCs statistics:"
-for i in precondition postcondition "loop invariant" "loop variant" assertion division overflow range index; do
+for i in precondition postcondition "loop invariant" "loop variant" assertion division overflow range index discriminant; do
   CNT=`grep "${i}" ${FILE} | grep -v "saved VC" | wc -l`
   OK=` grep "${i}" ${FILE} | grep -v "saved VC" | grep -v "not proved" | wc -l`
   echo ${i} ${OK}/${CNT}
@@ -19,7 +19,7 @@ echo Total: ${OK_SUM}/${SUM}
 
 # LaTeX table output
 echo "LaTeX table line:"
-for i in precondition postcondition "loop invariant" "loop variant" assertion division int_overflow float_overflow range index; do
+for i in precondition postcondition "loop invariant" "loop variant" assertion division int_overflow float_overflow range index discriminant; do
   CNT=`grep "${i}" ${FILE} | grep -v "saved VC" | wc -l`
   echo -n " & ${CNT}"
 done
@@ -29,12 +29,13 @@ echo
 
 echo
 
-INTEGER_OVERFLOWS=`grep "\"GP_Reason:VC_OVERFLOW_CHECK\" \"GP_Sloc_VC:[a-zA-Z_]\+.ad[bs]:[0-9]\+:[0-9]\+\" \"keep_on_simp\" (Standard__integer" algorithm/obj/gnatprove/*.mlw -o |uniq|wc -l`
-CONTAINER_OVERFLOWS=`grep "\"GP_Reason:VC_OVERFLOW_CHECK\" \"GP_Sloc_VC:[a-zA-Z_]\+.ad[bs]:[0-9]\+:[0-9]\+\" \"keep_on_simp\" (Ada__container" algorithm/obj/gnatprove/*.mlw -o |uniq|wc -l`
-FLOAT_OVERFLOWS=`grep "\"GP_Reason:VC_OVERFLOW_CHECK\" \"GP_Sloc_VC:[a-zA-Z_]\+.ad[bs]:[0-9]\+:[0-9]\+\" \"keep_on_simp\" (Standard__float" algorithm/obj/gnatprove/*.mlw -o |uniq|wc -l`
+INTEGER_OVERFLOWS=`grep "\"GP_Reason:VC_OVERFLOW_CHECK\" \"GP_Sloc_VC:[a-zA-Z_-]\+.ad[bs]:[0-9]\+:[0-9]\+\" \"keep_on_simp\" (Standard__integer" algorithm/obj/gnatprove/*.mlw -o |uniq|wc -l`
+CONTAINER_OVERFLOWS=`grep "\"GP_Reason:VC_OVERFLOW_CHECK\" \"GP_Sloc_VC:[a-zA-Z_-]\+.ad[bs]:[0-9]\+:[0-9]\+\" \"keep_on_simp\" (Ada__container" algorithm/obj/gnatprove/*.mlw -o |uniq|wc -l`
+FLOAT_OVERFLOWS=`grep "\"GP_Reason:VC_OVERFLOW_CHECK\" \"GP_Sloc_VC:[a-zA-Z_-]\+.ad[bs]:[0-9]\+:[0-9]\+\" \"keep_on_simp\" (Standard__float" algorithm/obj/gnatprove/*.mlw -o |uniq|wc -l`
 
-echo "integer overflows: ${INTEGER_OVERFLOWS}"
-echo "container overflows: ${CONTAINER_OVERFLOWS}"
+TOTAL_INT_OVERFLOWS=`expr ${INTEGER_OVERFLOWS} + ${CONTAINER_OVERFLOWS}`
+
+echo "integer overflows: ${INTEGER_OVERFLOWS} + ${CONTAINER_OVERFLOWS} = ${TOTAL_INT_OVERFLOWS}"
 echo "floating-point overflows: ${FLOAT_OVERFLOWS}"
 TOTAL_OVERFLOWS=`expr ${INTEGER_OVERFLOWS} + ${FLOAT_OVERFLOWS}`
 echo "total overflows: ${TOTAL_OVERFLOWS}"
