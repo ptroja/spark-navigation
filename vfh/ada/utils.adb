ed7763fbf261a6c35876cbbe2210f16547c59dd9
Franck Vedrine <Franck.Vedrine@cea.fr>
2014-02-21 18:00:19 +0100

    [Fluctuat] local modification on utils for Fluctuat - do not compile

--- vfh/ada/utils.adb
+++ vfh/ada/utils.adb
@@ -1,10 +1,20 @@
-with Ada.Numerics;
+with Ada.Numerics.Generic_Elementary_Functions;
 
 package body Utils is
+   package Elem_Fun is new ADA.Numerics.Generic_Elementary_Functions(Float);
 
    function DTOR (D : Float) return Float is
    begin
-      return (D * Ada.Numerics.Pi / 180.0);
+      return (D * Pi / 180.0);
    end DTOR;
 
+   function rint (X : Float) return Integer is
+   begin
+     return Integer (Float'Unbiased_Rounding (X));
+   end rint;
+
+   function Hypot (X, Y : Float) return Float is
+   begin
+     return Elem_Fun.sqrt(X*X + Y*Y);
+   end Hypot;
 end Utils;
--- vfh/ada/utils.ads
+++ vfh/ada/utils.ads
@@ -1,13 +1,8 @@
 package Utils is
 
+   Pi : constant := 3.1415926535_8979323846_2643383279_5028841972;
    function DTOR (D : Float) return Float;
-
-   function rint (X : Float) return Integer is
-     (Integer (Float'Unbiased_Rounding (X)));
-
-   function Hypot (X, Y : Float) return Float
-   with
-     Post => Hypot'Result >= 0.0;
-   pragma Import (C, Hypot, "hypotf");
+   function rint (X : Float) return Integer;
+   function Hypot (X, Y : Float) return Float;
 
 end Utils;
